5c873d2bc316ad4fee8c3a939b406b72d08eb349
srini <srinibaj10@gmail.com>
2025-04-05 13:34:12 -0400

    Add files via upload

---
+++ app/(app)/chat/page.tsx
@@ -0,0 +1,11 @@
+import { ChatLayout } from "@/components/chat/chat-layout"
+import { EmptyState } from "@/components/chat/empty-state"
+
+export default function ChatPage() {
+  return (
+    <ChatLayout>
+      <EmptyState />
+    </ChatLayout>
+  )
+}
+
